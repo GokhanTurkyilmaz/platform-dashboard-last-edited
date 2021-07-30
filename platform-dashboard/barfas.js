0a235c6c21a0e5fb416b091b719fe3879ef5201c
emir-91 <emir.hf91@gmail.com>
2021-07-30 10:49:31 +0300

    added id for close button in modal

--- platform-dashboard/barfas.js
+++ platform-dashboard/barfas.js
@@ -289,7 +289,7 @@ function editSideBarMenuItmes() {
         '<div class="modal-content">'+
             '<div class="modal-header">'+
                 '<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>'+
-                '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                '<button id="btnClose" type="button" class="close" data-dismiss="modal" aria-label="Close">'+
                     '<span aria-hidden="true">&times;</span>'+
                 '</button> </div>'+
             '<div class="modal-body"><form>'+
